a8ca7cb34427ee587d1c8725a9c7c969d5548392
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2023-10-24 22:51:37 +0200

    Add utility to make valid URLs

---
+++ src/resources/js/shared/helpers/urlExtractor.ts
@@ -0,0 +1,23 @@
+export function extractDomain(url: string) {
+    const result = url.match(
+        /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/im,
+    );
+
+    if (!result) throw new Error("Invalid URL provided.");
+
+    return result[1];
+}
+
+export function extractProtocol(url: string, defaultProtocol = "https://") {
+    const result = url.match(
+        /^(https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?(?:[^:\/\n]+)/im,
+    );
+
+    return (result && result[1]) ?? defaultProtocol;
+}
+
+export function makeCanonical(url: string) {
+    const domain = extractDomain(url);
+    const protocol = extractProtocol(url);
+    return protocol + domain;
+}
