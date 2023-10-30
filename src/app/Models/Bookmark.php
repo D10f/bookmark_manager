74ec8803d8f4bbef122ff60f6e4c86a514fd0f45
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2023-10-31 00:04:03 +0100

    Moved FaviconController logic to Events. Add event dispatcher on bookmark modal

--- src/app/Models/Bookmark.php
+++ src/app/Models/Bookmark.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\BookmarkProcessed;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -20,6 +21,16 @@ class Bookmark extends Model
         'category',
         'favicon_url',
         'user_id'
+    ];
+
+    /**
+     * The event map for the model.
+     *
+     * @var array<string, event>
+     */
+    protected $dispatchesEvents = [
+        'creating' => BookmarkProcessed::class,
+        'updating' => BookmarkProcessed::class,
     ];
 
     /**
