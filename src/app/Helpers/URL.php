e50725d3c05eb21fed84b0d9bb03043432d08e03
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2023-11-13 00:14:50 +0100

    Updated exception thrown. Add path property

--- src/app/Helpers/URL.php
+++ src/app/Helpers/URL.php
@@ -2,24 +2,24 @@
 
 namespace App\Helpers;
 
-use Exception;
-
 class URL
 {
     public $scheme;
     public $hostname;
+    public $path;
     public $canonical;
 
     public function __construct(string $url)
     {
         if (!filter_var($url, FILTER_VALIDATE_URL))
         {
-            throw new Exception('Invalid URL provided.');
+            throw new \InvalidArgumentException('Invalid URL provided.');
         }
 
         $parsed_url = parse_url($url);
         $this->scheme = $parsed_url['scheme'];
         $this->hostname = $parsed_url['host'];
+        $this->path = $parsed_url['path'] ?? '';
         $this->canonical = $parsed_url['scheme'] . '://' . $parsed_url['host'];
     }
 }
