4f90c9a0d3b9cc1372e6943e08ba05de8f948ce3
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2023-12-20 21:13:12 +0100

    Removed link to settings as a future feature to implement

--- src/resources/js/components/TheNavbar.vue
+++ src/resources/js/components/TheNavbar.vue
@@ -1,32 +1,27 @@
 <template>
     <header
-        class="relative z-50 font-bold text-md text-white bg-slate-950 border-b border-b-slate-700 rounded-md shadow-y shadow-sm shadow-slate-900 p-4 flex justify-between items-center"
-    >
+        class="relative z-50 font-bold text-md text-white bg-slate-950 border-b border-b-slate-700 rounded-md shadow-y shadow-sm shadow-slate-900 p-4 flex justify-between items-center">
         <h3>Welcome back, Test User!</h3>
 
         <div class="flex gap-2 text-xl">
             <Link href="#">
-                <Tooltip tooltip="Search bookmarks" position="bottom">
-                    <IconSearch
-                        class="flex justify-center items-center hover:bg-slate-600 w-8 h-8 p-2 rounded-full"
-                    />
-                </Tooltip>
+            <Tooltip tooltip="Search bookmarks" position="bottom">
+                <IconSearch class="flex justify-center items-center hover:bg-slate-600 w-8 h-8 p-2 rounded-full" />
+            </Tooltip>
             </Link>
 
-            <Link href="#">
-                <Tooltip tooltip="Settings" position="bottom">
-                    <IconCog
-                        class="flex justify-center items-center hover:bg-slate-600 w-8 h-8 p-2 rounded-full"
-                    />
-                </Tooltip>
-            </Link>
+            <!-- <Link href="#"> -->
+            <!--     <Tooltip tooltip="Settings" position="bottom"> -->
+            <!--         <IconCog -->
+            <!--             class="flex justify-center items-center hover:bg-slate-600 w-8 h-8 p-2 rounded-full" -->
+            <!--         /> -->
+            <!--     </Tooltip> -->
+            <!-- </Link> -->
 
             <Link href="#">
-                <Tooltip tooltip="Account" position="bottom">
-                    <IconProfile
-                        class="flex justify-center items-center hover:bg-slate-600 w-8 h-8 p-2 rounded-full"
-                    />
-                </Tooltip>
+            <Tooltip tooltip="Account" position="bottom">
+                <IconProfile class="flex justify-center items-center hover:bg-slate-600 w-8 h-8 p-2 rounded-full" />
+            </Tooltip>
             </Link>
 
             <!-- <button class="flex justify-center items-center hover:bg-slate-599 w-8 h-8 p-2 rounded-full"> -->
