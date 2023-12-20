abfbbe22baa25c73db2b85064075116b6f79bf8d
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2023-12-20 22:11:41 +0100

    Add the category store, mainly as replacement for bookmark store (to be deleted at some point, probably)

--- src/resources/js/stores/category.ts
+++ src/resources/js/stores/category.ts
@@ -1,27 +1,64 @@
 import { computed, ref } from "vue";
 import { defineStore } from "pinia";
-import { Category } from "@/types/category";
+import { getCookie } from "@/helpers/api";
 
 export const useCategoryStore = defineStore("category", () => {
-    const categories = ref<Category[]>([]);
+    const categories = ref<App.Models.Category[]>([]);
 
     const topLevelCategories = computed(() =>
         categories.value.filter((c) => c.parent_id === null),
     );
 
-    function loadCategories(newCategories: Category[]) {
-        categories.value = newCategories;
-    }
+    const categoryNames = computed(() =>
+        categories.value
+            .map((category) => ({
+                value: category.id,
+                label: categoryFQDN(category).join("/"),
+            }))
+            .sort((a, b) => a.label.localeCompare(b.label)),
+    );
 
-    function addCategory(category: Category) {
+    async function createCategory(
+        categoryName: string,
+    ): Promise<App.Models.Category> {
+        let parent_id: number | null = null;
+
+        if (isFQDN(categoryName)) {
+            const tree = categoryName.split("/").slice(0, -1);
+
+            if (!tree.length) {
+                parent_id = null;
+            }
+
+            const treeStr = tree.join("/");
+            const parent = categoryNames.value.find((c) => c.label === treeStr);
+
+            if (parent) {
+                parent_id = parent?.value;
+            }
+        }
+
+        const res = await fetch("/api/categories/create", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                "X-XSRF-TOKEN": getCookie("XSRF-TOKEN"),
+            },
+            body: JSON.stringify({
+                title: categoryName.split("/").slice(-1)[0],
+                parent_id,
+            }),
+        });
+
+        const category = await res.json();
         categories.value.push(category);
+        return category;
     }
 
-    function getSubcategories(category: Category) {
-        return categories.value.filter((c) => c.parent_id === category.id);
-    }
-
-    function getCategoryTree(category: Category): Category[] {
+    function getCategoryTree(
+        category: App.Models.Category,
+    ): App.Models.Category[] {
         if (category.parent_id === null) {
             return [category];
         }
@@ -33,17 +70,20 @@ export const useCategoryStore = defineStore("category", () => {
         return [...getCategoryTree(parent!), category];
     }
 
-    function categoryFQDN(category: Category): string[] {
-        const tree = getCategoryTree(category);
-        return tree.map((category) => category.title);
+    function categoryFQDN(category: App.Models.Category): string[] {
+        return getCategoryTree(category).map((c) => c.title);
+    }
+
+    function isFQDN(categoryName: string) {
+        const re = new RegExp(/(?<!\\)\//);
+        return re.test(categoryName);
     }
 
     return {
         categories,
         topLevelCategories,
-        loadCategories,
-        addCategory,
-        getSubcategories,
+        categoryNames,
+        createCategory,
         getCategoryTree,
         categoryFQDN,
     };
