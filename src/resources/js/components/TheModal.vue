33aa163a135b52c9dc353c37455f4eecdef82e36
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2023-12-24 16:50:34 +0100

    Increase z-index value

--- src/resources/js/components/TheModal.vue
+++ src/resources/js/components/TheModal.vue
@@ -11,7 +11,7 @@ defineEmits<{
     <Transition enter-from-class="opacity-0 scale-125" enter-to-class="opacity-1 scale-100"
         enter-active-class="transition duration-300" leave-active-class="transition duration-200"
         leave-from-class="opacity-1" leave-to-class="opacity-0">
-        <aside v-show="show" class="flex justify-center items-center fixed inset-0 bg-slate-800/80 backdrop-blur-sm z-20">
+        <aside v-show="show" class="flex justify-center items-center fixed inset-0 bg-slate-800/80 backdrop-blur-sm z-50">
             <div class="bg-slate-700 text-white p-4 border border-white rounded-md w-[90%] max-w-[500px]">
                 <header class="flex justify-between items-center text-2xl mb-2">
                     <slot name="header" />
