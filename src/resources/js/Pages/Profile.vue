c7839b7d19bcea351374a4174af04bf2308eca81
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2024-04-28 00:03:42 +0200

    Updated Profile page to start work on import/export functionality for bookmarks. Add bookmark parser

--- src/resources/js/Pages/Profile.vue
+++ src/resources/js/Pages/Profile.vue
@@ -1,12 +1,14 @@
 <template>
     <aside class="flex justify-end items-center mt-2 z-10">
+        <BaseButton intent="text">Collapse All</BaseButton>
+
         <BaseButton as="Link" :href="home_url">
             <IconChevron class="rotate-90 fill-current w-4 h-4" />
             Back
         </BaseButton>
     </aside>
 
-    <CardContainer title="Edit Profile">
+    <CardContainer title="Profile" collapsable>
         <template #actions>
             <!-- <button @click="logoutProfile" aria-label="Logout"> -->
             <!--     <Tooltip tooltip="Logout" aria-hidden="true"> -->
@@ -21,7 +23,7 @@
                 </Tooltip>
             </BaseButton>
 
-            <BaseButton intent="rounded" @click="toggleDeletePrompt" aria-label="delete account">
+            <BaseButton intent="rounded" @click="showDeleteModal = true" aria-label="delete account">
                 <Tooltip tooltip="Delete account">
                     <IconTrash v-if="!showDeleteConfirmation" class="w-8 h-8 p-2" />
                     <div v-else class="w-8 h-8 text-2xl">&times;</div>
@@ -45,6 +47,42 @@
             <!--         </template> -->
             <!--     </Modal> -->
             <!-- </Teleport> -->
+
+            <Teleport to="body">
+                <Modal :show="showDeleteModal" @close-modal="showDeleteModal = false">
+                    <template #header>
+                        <h3 class="font-md">Delete Account?</h3>
+                    </template>
+
+                    <template #footer>
+                        <div
+                            class="m-4 px-4 py-2 border-l-2 border-red-400 bg-gray-100/20 rounded-tr-md rounded-br-md text-red-200">
+                            <h3 class="text-2xl font-bold">Caution!</h3>
+                            <p class="text-lg">This action cannot be undone.</p>
+                        </div>
+
+                        <form class="px-4 py-2" @submit.prevent="deleteProfile">
+                            <div class="py-2 flex flex-col gap-1">
+                                <BaseInput label="Enter the following text below to proceed:" disabled
+                                    v-model="deleteForm.validationPhrase"
+                                    class="font-bold uppercase bg-transparent !text-cyan-400 text-2xl px-2 pt-2" />
+                            </div>
+
+                            <div class="py-2 flex flex-col gap-1">
+                                <BaseInput label="Confirm" v-model="deleteForm.confirmValidation" class="uppercase" />
+                            </div>
+
+                            <div class="py-2">
+                                <BaseButton :loading="form.processing" :disabled="!isDeleteBtnActive" class="mt-2"
+                                    type="submit">
+                                    Delete Profile
+                                    <template #loading> ... </template>
+                                </BaseButton>
+                            </div>
+                        </form>
+                    </template>
+                </Modal>
+            </Teleport>
         </template>
 
         <form class="px-4 py-2" @submit.prevent="updateProfile">
@@ -74,40 +112,85 @@
         </form>
     </CardContainer>
 
-    <Transition enter-from-class="opacity-0 translate-y-4" enter-to-class="opacity-1 translate-y-0"
-        enter-active-class="transition duration-300" leave-active-class="transition duration-200"
-        leave-from-class="opacity-1 translate-y-0" leave-to-class="opacity-0 translate-y-4">
-        <CardContainer title="Delete Profile" v-show="showDeleteConfirmation">
-            <div class="m-4 px-4 py-2 border-l-2 border-red-400 bg-gray-100/20 rounded-tr-md rounded-br-md text-red-200">
-                <h3 class="text-2xl font-bold">Caution!</h3>
-                <p class="text-lg">This action cannot be undone.</p>
+    <CardContainer title="Appearance" collapsable>
+        <!-- TODO:
+            Card container transparency control
+            Black & white filter for favicons
+            Toggle favicons
+            Default folder favicon
+            Default missing favicon
+            Theme
+            Background (color, image, blobs, etc)
+        -->
+    </CardContainer>
+
+    <CardContainer title="Shortcuts" collapsable>
+        <!-- TODO:
+            Ctrl + K for search
+            Ctrl + A to add new bookmark
+            Ctrl + B to import / export
+            Ctrl + 1 to launch <insert_url>
+        -->
+    </CardContainer>
+
+    <CardContainer title="Import / Export" collapsable>
+        <!-- TODO:
+            Import / Export Bookmarks
+        -->
+        <div class="px-4 py-2">
+            <div class="py-2">
+                <p>
+                    Import bookmarks from another browser backup (HTML file
+                    only).
+                </p>
+                <input type="file" class="invisible w-0 h-0" aria-hidden ref="htmlImport" @change="handleFilePicker" />
+                <BaseButton class="my-2" @click="() => htmlImport.click()">Import</BaseButton>
             </div>
 
-            <form class="px-4 py-2" @submit.prevent="deleteProfile">
-                <div class="py-2 flex flex-col gap-1">
-                    <BaseInput label="Enter the following text below to proceed:" disabled
-                        v-model="deleteForm.validationPhrase"
-                        class="font-bold uppercase bg-transparent !text-cyan-400 text-2xl px-2 pt-2" />
-                </div>
+            <div class="py-2">
+                <p>Export bookmark data ready to be used by browsers.</p>
+                <BaseButton class="my-2">Export</BaseButton>
+            </div>
+        </div>
+    </CardContainer>
 
-                <div class="py-2 flex flex-col gap-1">
-                    <BaseInput label="Confirm" v-model="deleteForm.confirmValidation" class="uppercase" />
-                </div>
+    <!-- <Transition enter-from-class="opacity-0 translate-y-4" enter-to-class="opacity-1 translate-y-0" -->
+    <!--     enter-active-class="transition duration-300" leave-active-class="transition duration-200" -->
+    <!--     leave-from-class="opacity-1 translate-y-0" leave-to-class="opacity-0 translate-y-4"> -->
+    <!--     <CardContainer title="Delete Profile" v-show="showDeleteConfirmation"> -->
+    <!--         <div -->
+    <!--             class="m-4 px-4 py-2 border-l-2 border-red-400 bg-gray-100/20 rounded-tr-md rounded-br-md text-red-200"> -->
+    <!--             <h3 class="text-2xl font-bold">Caution!</h3> -->
+    <!--             <p class="text-lg">This action cannot be undone.</p> -->
+    <!--         </div> -->
 
-                <div class="py-2">
-                    <BaseButton :loading="form.processing" :disabled="!isDeleteBtnActive" class="mt-2" type="submit">
-                        Delete Profile
-                        <template #loading> ... </template>
-                    </BaseButton>
-                </div>
-            </form>
-        </CardContainer>
-    </Transition>
+    <!--         <form class="px-4 py-2" @submit.prevent="deleteProfile"> -->
+    <!--             <div class="py-2 flex flex-col gap-1"> -->
+    <!--                 <BaseInput label="Enter the following text below to proceed:" disabled -->
+    <!--                     v-model="deleteForm.validationPhrase" -->
+    <!--                     class="font-bold uppercase bg-transparent !text-cyan-400 text-2xl px-2 pt-2" /> -->
+    <!--             </div> -->
+
+    <!--             <div class="py-2 flex flex-col gap-1"> -->
+    <!--                 <BaseInput label="Confirm" v-model="deleteForm.confirmValidation" class="uppercase" /> -->
+    <!--             </div> -->
+
+    <!--             <div class="py-2"> -->
+    <!--                 <BaseButton :loading="form.processing" :disabled="!isDeleteBtnActive" class="mt-2" type="submit"> -->
+    <!--                     Delete Profile -->
+    <!--                     <template #loading> ... </template> -->
+    <!--                 </BaseButton> -->
+    <!--             </div> -->
+    <!--         </form> -->
+    <!--     </CardContainer> -->
+    <!-- </Transition> -->
 </template>
 
 <script setup lang="ts">
 import { computed, ref } from "vue";
 import { useForm } from "@inertiajs/vue3";
+import { getCookie } from "@/helpers/api";
+import BookmarkParser from "@/helpers/bookmarks";
 import BaseButton from "@/components/BaseButton.vue";
 import BaseInput from "@/components/BaseInput.vue";
 import CardContainer from "@/components/CardContainer.vue";
@@ -140,11 +223,11 @@ function updateProfile() {
 
 /** ------------------------ Delete --------------------------------- */
 
-const showDeleteConfirmation = ref(false);
+const showDeleteModal = ref(false);
 
-function toggleDeletePrompt() {
-    showDeleteConfirmation.value = !showDeleteConfirmation.value;
-}
+// function toggleDeletePrompt() {
+//     showDeleteConfirmation.value = !showDeleteConfirmation.value;
+// }
 
 const isDeleteBtnActive = computed(
     () =>
@@ -165,6 +248,26 @@ function deleteProfile() {
 
 function logoutProfile() {
     form.post(props.logout_url);
+}
+
+/** ------------------------ Import bookmarks ----------------------- */
+
+const htmlImport = ref(null);
+
+function handleFilePicker(event) {
+    const file = event.target.files[0];
+
+    if (file.size > 5_000_000) {
+        console.log("File size cannot exceed 5MB.");
+        return;
+    }
+
+    if (file.type != "text/html") {
+        console.log("Invalid file type: only HTML files are supported.");
+        return;
+    }
+
+    new BookmarkParser(file).parse().then(console.log).catch();
 }
 </script>
 
---
+++ src/resources/js/helpers/bookmarks.ts
@@ -0,0 +1,87 @@
+export default class BookmarkParser {
+    constructor(private bookmarkFile: File) { }
+
+    async parse() {
+        const bookmarks = {
+            type: "Category",
+            bookmarks: [],
+        };
+
+        const rootNode = await this.read();
+        this._parse(rootNode as Node, bookmarks);
+        return bookmarks;
+    }
+
+    private _parse(root: Node, tree: Record<string, any>) {
+        for (const node of root.childNodes) {
+            if (node.nodeName != "DT") continue;
+
+            const nextNode = this.peek(node);
+
+            if (!nextNode) continue;
+
+            if (nextNode.type === "category") {
+                const dl = (node as HTMLElement).querySelector("dl")!;
+                tree[nextNode.title] = {
+                    bookmarks: nextNode.bookmarks,
+                };
+                this._parse(dl, tree[nextNode.title]);
+            } else if (nextNode.type === "bookmark") {
+                tree.bookmarks.push({
+                    title: nextNode.title,
+                    url: nextNode.url,
+                });
+            }
+        }
+    }
+
+    private peek(node: Node) {
+        const nextNode = node.childNodes[0] as HTMLElement;
+
+        if (nextNode.nodeName === "H3") {
+            return {
+                type: "category",
+                title: nextNode.textContent as string,
+                bookmarks: [],
+            };
+        }
+
+        if (nextNode.nodeName === "A") {
+            return {
+                type: "bookmark",
+                title: nextNode.textContent as string,
+                url: nextNode.getAttribute("HREF"),
+            };
+        }
+
+        return null;
+    }
+
+    private read() {
+        const reader = new FileReader();
+
+        return new Promise((resolve, reject) => {
+            reader.onload = () => {
+                if (typeof reader.result !== "string") {
+                    return reader.abort();
+                }
+
+                if (
+                    !reader.result.startsWith(
+                        "<!DOCTYPE NETSCAPE-Bookmark-file-1>",
+                    )
+                ) {
+                    return reject("Invalid file declaration.");
+                }
+
+                const parser = new DOMParser();
+                const doc = parser.parseFromString(reader.result, "text/html");
+                resolve(doc.querySelector("dl"));
+            };
+
+            reader.onabort = () => reject("Error reading file.");
+
+            reader.readAsText(this.bookmarkFile);
+        });
+    }
+}
