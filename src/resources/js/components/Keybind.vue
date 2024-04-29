b4c4c2f587881cc00a514e91c51b50710a626bd5
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2024-04-29 20:07:17 +0200

    Add new Keybind component to display and handle custom key shortcut combinations

---
+++ src/resources/js/components/Keybind.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex justify-between items-center py-1">
+        <slot />
+        <span class="pointer-events-none">
+            <template v-for="(key, idx) in keys">
+                <kbd
+                    class="text-sm text-center uppercase min-w-[32px] p-1 inline-block font-semibold rounded border-b-2 bg-slate-600 border-b-gray-700">
+                    {{ key }}
+                </kbd>
+                {{ idx + 1 < keys.length ? " + " : "" }} </template>
+        </span>
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    keys: string[];
+}>();
+</script>
